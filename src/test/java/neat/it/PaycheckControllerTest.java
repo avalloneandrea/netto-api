b6241af949ac5ad64c0ecfda471e4f0efecdb9ce
Andrea Avallone <avalloneandrea@gmail.com>
2019-04-06 12:07:55 +0200

    Add PaycheckControllerTest.java

---
+++ src/test/java/neat/it/PaycheckControllerTest.java
@@ -0,0 +1,71 @@
+package neat.it;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PaycheckController.class)
+@ExtendWith(SpringExtension.class)
+public class PaycheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @ParameterizedTest
+    @CsvSource({"-1", "0", "1"})
+    public void getPaycheckWithValidAdditionalSalaries(int input) throws Exception {
+        this.mockMvc
+                .perform(get("/it/paycheck?additionalSalaries=" + input))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1.5", "two"})
+    public void getPaycheckWithInvalidAdditionalSalaries(String input) throws Exception {
+        this.mockMvc
+                .perform(get("/it/paycheck?additionalSalaries=" + input))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-20000.00", "0", "20000.00", "20000.50"})
+    public void getPaycheckWithValidGrossIncome(BigDecimal input) throws Exception {
+        this.mockMvc
+                .perform(get("/it/paycheck?grossIncome=" + input))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"twentythousand"})
+    public void getPaycheckWithInvalidGrossIncome(String input) throws Exception {
+        this.mockMvc
+                .perform(get("/it/paycheck?grossIncome=" + input))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"-600.00", "0", "600.00", "600.50"})
+    public void getPaycheckWithValidNetBonus(BigDecimal input) throws Exception {
+        this.mockMvc
+                .perform(get("/it/paycheck?netBonus=" + input))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"sixhundred"})
+    public void getPaycheckWithInvalidNetBonus(String input) throws Exception {
+        this.mockMvc
+                .perform(get("/it/paycheck?netBonus=" + input))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
